1d683cba37b4d576c5306af2e4c3a04121b55e7b
yolo <1573786401@qq.com>
2025-11-12 17:54:57 +0800

    滑动窗口修复

--- react/src/components/ui/scroll-area.tsx
+++ react/src/components/ui/scroll-area.tsx
@@ -16,12 +16,12 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn('relative', className)}
+      className={cn('relative overflow-hidden', className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
         data-slot="scroll-area-viewport"
-        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+        className="focus-visible:ring-ring/50 h-full w-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
         ref={viewportRef}
       >
         {children}
@@ -44,9 +44,9 @@ function ScrollBar({
       className={cn(
         'flex touch-none p-px transition-colors select-none',
         orientation === 'vertical' &&
-          'h-full w-2.5 border-l border-l-transparent',
+        'h-full w-2.5 border-l border-l-transparent',
         orientation === 'horizontal' &&
-          'h-2.5 flex-col border-t border-t-transparent',
+        'h-2.5 flex-col border-t border-t-transparent',
         className
       )}
       {...props}
